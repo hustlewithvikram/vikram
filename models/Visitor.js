a9597262183335fb946ff800d276930b3f1d7300
Vikram Vishwakarma <vs423502@gmail.com>
2024-09-01 23:47:04 +0530

    ok its working

--- models/Visitor.js
+++ models/Visitor.js
@@ -1,9 +1,15 @@
 // models/Visitor.js
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const visitorSchema = new mongoose.Schema({
-  ipAddress: { type: String, required: true },
+  ipAddress: {
+    type: String,
+    required: true,
+    unique: true, // Ensure each IP address is unique
+    trim: true, // Trim any extra spaces
+  },
   visitDate: { type: Date, default: Date.now },
 });
 
-export default mongoose.models.Visitor || mongoose.model('Visitor', visitorSchema);
+export default mongoose.models.Visitor ||
+  mongoose.model("Visitor", visitorSchema);
