e4b55fd035351a1acaf66360c2f173ccaadff015
Vikram Vishwakarma <vs423502@gmail.com>
2024-09-02 12:28:17 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,11 +10,17 @@ export const metadata: Metadata = {
   description: "Made with Next Js",
 };
 
+interface VisitorType {
+  _id: string;
+  ipAddress: string;
+  visitDate: string;
+}
+
 export default async function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   // Fetch visitor data
-  const visitors = await fetchVisitors();
+  const visitors: VisitorType[] = await fetchVisitors();
 
   return (
     <html lang="en">
--- pages/api/visitors.ts
+++ pages/api/visitors.ts
@@ -1,12 +1,51 @@
 // pages/api/visitors.ts
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { fetchVisitors } from '@/lib/fetchVisitors';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { connectToDatabase } from "@/lib/mongoose";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+let collectionName = "visitors";
+
+async function fetchVisitors() {
   try {
+    const db = await connectToDatabase();
+    const collection = db.collection(collectionName);
+    return await collection.find({}).toArray();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function addVisitor(ip: string) {
+  try {
+    const db = await connectToDatabase();
+    const collection = db.collection(collectionName);
+    await collection.insertOne({ ip, timestamp: new Date() });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    // Get visitor's IP address
+    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+
+    // Fetch current visitors
     const visitors = await fetchVisitors();
+
+    // Check if IP is already in the database
+    const ipExists = visitors.some((visitor: { ip: string | string[] | undefined; }) => visitor.ip === ip);
+
+    // If IP doesn't exist, add it
+    if (!ipExists) {
+      await addVisitor(ip as string);
+    }
+
+    // Send the visitors as a response
     res.status(200).json(visitors);
   } catch (error) {
-    res.status(500).json({ message: 'Error fetching visitors' });
+    res.status(500).json({ message: "Error fetching or adding visitors" });
   }
 }
