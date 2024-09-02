2cfa2217d7641e2bd254a872fd56f0fe5252ccaa
Vikram Vishwakarma <vs423502@gmail.com>
2024-09-02 11:09:40 +0530

    check the visitor count

--- pages/api/visitors.js
+++ pages/api/visitors.js
@@ -2,19 +2,10 @@
 import { connectToDatabase } from "../../lib/mongoose";
 import Visitor from "../../models/Visitor";
 
+
 export default async function handler(req, res) {
   const { method } = req;
-
-  try {
-    const { db } = await connectToDatabase();
-    res
-      .status(200)
-      .json({ message: "Connected to database", db: db.databaseName });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Failed to connect to database", error: error.message });
-  }
+  const { db } = await connectToDatabase();
 
   if (method === "GET") {
     try {
@@ -25,14 +16,13 @@ export default async function handler(req, res) {
       // Check if the visitor already exists in the database
       let visitor = await Visitor.findOne({ ipAddress });
 
-      if (visitor) {
-        // If visitor exists, respond with a message
-        res.status(200).json({ message: "Visitor already logged", visitor });
-      } else {
-        // If visitor does not exist, create a new record
+      const visitorCount = await Visitor.countDocuments();
+      res.status(400).json({totalCount : visitorCount});
+
+      if (!visitor) {
         visitor = await Visitor.create({ ipAddress });
-        res.status(201).json({ message: "New visitor logged", visitor });
       }
+
     } catch (error) {
       // Handle any errors
       res.status(500).json({ message: "Error logging visitor", error });
